034ec4528168451395a0ffe3ba395a701024f775
luckyswap.finance@gmail.com <luckyswap.finance@gmail.com>
2021-05-21 09:30:10 +0700

    add

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -14,22 +14,12 @@ const farms: FarmConfig[] = [
   },
   {
     pid: 1,
-    lpSymbol: 'BES-DEMO-BNB LP',
+    lpSymbol: 'LUCKY-BNB LP',
     lpAddresses: {
       97: '0xd08A7AfF7952Aaab598121Ad8Ee28e5CC55CFaA1',
       56: '0xA527a61703D82139F8a06Bc30097cC9CAA2df5A6',
     },
     token: tokens.bdex,
-    quoteToken: tokens.wbnb,
-  },
-  {
-    pid: 3,
-    lpSymbol: 'BEDEX-BNB LP',
-    lpAddresses: {
-      97: '0x44df2f78b2f8a72b15ac317c02b767cea8637477',
-      56: '0xb4d63ddf0d16538119269e0894f2122f85b13269',
-    },
-    token: tokens.bdextest,
     quoteToken: tokens.wbnb,
   }
 ]
