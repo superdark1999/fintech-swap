d00df52d86edf1dbad9fc43e2f30d5b2f520d068
Mac <mac@Macs-MacBook-Pro.local>
2021-05-26 17:59:32 +0700

    build

--- src/components/Swap/Modal/index.tsx
+++ src/components/Swap/Modal/index.tsx
@@ -52,7 +52,7 @@ const StyledDialogContent = styled(({ minHeight, maxHeight, mobile, isOpen, ...r
         min-height: ${minHeight}vh;
       `}
     @media (max-width: 768px) {
-      min-height: 60vh;
+      min-height: 65vh;
     }
 
     display: flex;
