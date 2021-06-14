386215cc8de74b140f93ae5e00df1733d98bc3e5
Thinh Tran <qthinh1995@gmail.com>
2021-06-15 06:37:49 +0700

    format

--- src/components-v2/UploadMedia/index.js
+++ src/components-v2/UploadMedia/index.js
@@ -1,110 +1,101 @@
-import React, { useState } from "react";
-import { Button,  Upload, Image } from "antd";
+import React, { useState } from 'react'
+import { Button, Upload, Image } from 'antd'
 import { CloseCircleFilled } from '@ant-design/icons'
 import styled from 'styled-components'
-
 
 const UploadForm = (props) => {
   const [imageUrl, setUrlImage] = useState(false)
   const [type, setType] = useState(false)
   const [file, setFile] = useState(false)
 
-
   const beforeUpload = (file) => {
     console.log('file: ', file.type)
-      getBase64(file)
-      setType(file.type)
-      setFile(file)
-      return false;
-  };
+    getBase64(file)
+    setType(file.type)
+    setFile(file)
+    return false
+  }
 
   const getBase64 = (img, callback) => {
-    const reader = new FileReader();
-    reader.addEventListener("load", () =>{
+    const reader = new FileReader()
+    reader.addEventListener('load', () => {
       props.onChange(reader.result)
       setUrlImage(reader.result)
-    }
-    );
-    
-    reader.readAsDataURL(img);
-  };
+    })
+
+    reader.readAsDataURL(img)
+  }
 
   const handleRemove = () => {
     setUrlImage(false)
     setFile(false)
-  };
+  }
 
-    return (
-      <UploadStyled >
-       { imageUrl && <CloseCircleFilled className="remove-image" onClick={handleRemove}/>}
-       { imageUrl &&  
-        (type.includes('video')) ? 
-          <video width="100%" controls>
-            <source
-              src={imageUrl}
-              type={file.type}
-            />
-            Your browser does not support HTML5 video.
-          </video>
-          :
-          <Image className="image" src={imageUrl} />
-        }
-       
+  return (
+    <UploadStyled>
+      {imageUrl && (
+        <CloseCircleFilled className="remove-image" onClick={handleRemove} />
+      )}
+      {imageUrl && type.includes('video') ? (
+        <video width="100%" controls>
+          <source src={imageUrl} type={file.type} />
+          Your browser does not support HTML5 video.
+        </video>
+      ) : (
+        <Image className="image" src={imageUrl} />
+      )}
+
       <Upload
-      {...props}
+        {...props}
         name="avatar"
         beforeUpload={beforeUpload}
         imageUrl={imageUrl}
         onRemove={handleRemove}
-        style={{width: '100%'}}
+        style={{ width: '100%' }}
         showUploadList={false}
       >
-           
-        { !imageUrl && (
+        {!imageUrl && (
           <div className="upload-image">
-            <Button>
-              Upload
-            </Button>
-            <p>Support: png / jpg / gif/ MP4 Size: ≤ 10MB
+            <Button>Upload</Button>
+            <p>
+              Support: png / jpg / gif/ MP4 Size: ≤ 10MB
               {/* Suggested ratio: 1:1 */}
-            </p>          
+            </p>
           </div>
-          )
-        }    
+        )}
       </Upload>
-      </UploadStyled>
-    )
+    </UploadStyled>
+  )
 }
 
-export default (UploadForm);
-
+export default UploadForm
 
 const UploadStyled = styled.div`
-  max-height: ${props => props.maxHeight || '350px'};
-  min-height:  350px;
-  max-width: ${props => props.maxWidth || '480px'};
+  max-height: ${(props) => props.maxHeight || '350px'};
+  min-height: 350px;
+  max-width: ${(props) => props.maxWidth || '480px'};
   background-color: #fff;
-  border: 1px dashed #E7EBEF;
+  border: 1px dashed #e7ebef;
   box-sizing: border-box;
   border-radius: 20px;
   position: relative;
   display: flex;
   justify-content: center;
   align-items: center;
-  span > .ant-upload-list-tex{
+  span > .ant-upload-list-tex {
     display: none;
   }
 
-  > video, > .ant-image > .ant-image-img {
+  > video,
+  > .ant-image > .ant-image-img {
     display: block;
-    max-height: ${props => props.maxHeight || '350px'};
-    max-width: ${props => props.maxWidth || '480px'};
+    max-height: ${(props) => props.maxHeight || '350px'};
+    max-width: ${(props) => props.maxWidth || '480px'};
     width: auto;
     height: auto;
   }
 
-  
-  >.remove-image {
+  > .remove-image {
     position: absolute;
     top: -5px;
     right: -5px;
@@ -114,18 +105,18 @@ const UploadStyled = styled.div`
     flex-direction: column;
     justify-content: center;
     align-items: center;
-   
-    >button {  
+
+    > button {
       border: 1px solid #333435;
       box-sizing: border-box;
       border-radius: 100px;
       padding: 0px 16px;
       width: 200px;
       margin-bottom: 5px;
-      }
-      >p{
-        width: 200px;
-        text-align: center
-      }
+    }
+    > p {
+      width: 200px;
+      text-align: center;
+    }
   }
-   `
+`
