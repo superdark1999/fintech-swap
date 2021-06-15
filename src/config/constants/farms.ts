1e7ed2fd357ecc15fae58647dfae705e8a7cad46
luckyswap.finance@gmail.com <luckyswap.finance@gmail.com>
2021-06-15 15:52:25 +0700

    deploy

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -4,16 +4,6 @@ import { FarmConfig } from './types'
 const farms: FarmConfig[] = [
   {
     pid: 0,
-    lpSymbol: 'CAKE',
-    lpAddresses: {
-      97: '0x9C21123D94b93361a29B2C2EFB3d5CD8B17e0A9e',
-      56: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82',
-    },
-    token: tokens.syrup,
-    quoteToken: tokens.wbnb,
-  },
-  {
-    pid: 1,
     lpSymbol: 'LUCKY-BNB LP',
     lpAddresses: {
       97: '0xd08A7AfF7952Aaab598121Ad8Ee28e5CC55CFaA1',
