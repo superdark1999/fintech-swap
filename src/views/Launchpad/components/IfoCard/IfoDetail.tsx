1ae413ff282862fb76a61432ee0bef52c6affe83
luckyswap.finance@gmail.com <luckyswap.finance@gmail.com>
2021-06-24 12:21:23 +0700

    add

--- src/views/Launchpad/components/IfoCard/IfoDetail.tsx
+++ src/views/Launchpad/components/IfoCard/IfoDetail.tsx
@@ -46,7 +46,7 @@ const IfoTitle = ({ activeIfo }: any) => {
   const [isApproved, setIsApproved] = useState(false)
   const [value, setValue] = useState('')
 
-  const {banner, sympol, description, name, address, currency, currencyAddress } = activeIfo
+  const {banner,social, sympol, description, name, address, currency, currencyAddress } = activeIfo
   const contract = useIfoContract(address)
   const { offeringAmount, raisingAmount, secondsUntilStart, secondsUntilEnd, status, startBlockNum } =
     useGetPublicIfoData(activeIfo)
@@ -207,7 +207,7 @@ const IfoTitle = ({ activeIfo }: any) => {
               <h3>{name}</h3>
 
               <BoxSocial>
-                <a href="/">
+                <a rel="noreferrer" target="_blank" href={social && social.twitter}>
                   <svg
                     version="1.1"
                     id="Capa_1"
@@ -231,7 +231,7 @@ const IfoTitle = ({ activeIfo }: any) => {
                     />
                   </svg>
                 </a>
-                <a href="/">
+                <a rel="noreferrer" target="_blank" href={social && social.medium}>
                   <svg
                     id="Bold"
                     enableBackground="new 0 0 24 24"
@@ -243,7 +243,7 @@ const IfoTitle = ({ activeIfo }: any) => {
                     <path d="m22.085 4.733 1.915-1.832v-.401h-6.634l-4.728 11.768-5.379-11.768h-6.956v.401l2.237 2.693c.218.199.332.49.303.783v10.583c.069.381-.055.773-.323 1.05l-2.52 3.054v.396h7.145v-.401l-2.52-3.049c-.273-.278-.402-.663-.347-1.05v-9.154l6.272 13.659h.729l5.393-13.659v10.881c0 .287 0 .346-.188.534l-1.94 1.877v.402h9.412v-.401l-1.87-1.831c-.164-.124-.249-.332-.214-.534v-13.467c-.035-.203.049-.411.213-.534z" />
                   </svg>
                 </a>
-                <a href="/">
+                <a rel="noreferrer" target="_blank" href={social && social.telegram}>
                   <svg
                     enableBackground="new 0 0 24 24"
                     height={512}
