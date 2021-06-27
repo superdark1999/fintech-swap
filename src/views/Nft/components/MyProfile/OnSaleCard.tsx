90cbf1303673ac9fbd95012464376353a4f94e95
musknguyen <musknguyen@gmail.com>
2021-06-27 11:53:14 +0700

    update UI

--- src/views/Nft/components/MyProfile/OnSaleCard.tsx
+++ src/views/Nft/components/MyProfile/OnSaleCard.tsx
@@ -1,21 +1,19 @@
-
 import React, { useEffect, useState } from 'react'
 import { UserProfileStyled, CartStyled, ListCart } from './styled'
 import Checkmark from 'assets/images/checkmark.svg'
-import { Row, Col, Tabs } from 'antd';
+import { Row, Col, Tabs } from 'antd'
 import Token from 'assets/images/token.svg'
 import Luckyswap from 'assets/images/luckyswap.svg'
-import usrMarketServices from 'services/web3Services/MarketServices';
+import usrMarketServices from 'services/web3Services/MarketServices'
 import _ from 'lodash'
 import { getPrice, getCompactString } from 'utils'
-
-export default function OnSaleCard({ data, }: any) {
+import { isMobile } from 'react-device-detect'
+export default function OnSaleCard({ data }: any) {
   const [loading, setLoading] = useState(true)
   const [price, setPrice] = useState(0)
   const marketService = usrMarketServices()
 
   useEffect(() => {
-
     const getPriceToken = async () => {
       if (data?.tokenId && marketService) {
         if (data?.NFTType == 'buy') {
@@ -24,14 +22,16 @@ export default function OnSaleCard({ data, }: any) {
           setPrice(price)
         } else {
           const bidsArr = await marketService?.getBidsByTokenId?.(data?.tokenId)
-          const bidsData = bidsArr?.map((item: any) => {
-            return {
-              key: item?.[0] || '',
-              address: item?.[0] || '',
-              price: Number(item?.[1]?._hex) / Number(1e18),
-            }
-          }) || []
-          const maxPrice = _.maxBy(bidsData, (item: any) => item?.price)?.price || 0
+          const bidsData =
+            bidsArr?.map((item: any) => {
+              return {
+                key: item?.[0] || '',
+                address: item?.[0] || '',
+                price: Number(item?.[1]?._hex) / Number(1e18),
+              }
+            }) || []
+          const maxPrice =
+            _.maxBy(bidsData, (item: any) => item?.price)?.price || 0
           const unitPrice = await marketService?.getTokenBidPrice(data?.tokenId)
           const price = getPrice(unitPrice?._hex)
           if (price > maxPrice) {
@@ -46,41 +46,61 @@ export default function OnSaleCard({ data, }: any) {
     getPriceToken()
   }, [data?.tokenId])
 
-
   if (loading) {
     return null
   }
   return (
     <CartStyled>
       <Row gutter={24}>
-        <Col xl={{ span: 7 }} md={{ span: 24 }} xs={{ span: 24 }} xxl={{ span: 7 }}>
-          <img className="avatar" src={data?.contentUrl} />
+        <Col
+          xl={{ span: 7 }}
+          md={{ span: 24 }}
+          xs={{ span: 24 }}
+          xxl={{ span: 7 }}
+        >
+          {data?.type === 'video' ? (
+            <video
+              muted
+              controls
+              autoPlay={isMobile ? false : true}
+              loop
+              className="avatar"
+            >
+              <source src={`${data?.contentUrl}#t=0.1`} type="video/mp4" />
+            </video>
+          ) : (
+            <img
+              className="avatar"
+              src={data?.contentUrl}
+              alt=""
+              loading="lazy"
+            />
+          )}
         </Col>
-        <Col className="description" xl={{ span: 16 }} md={{ span: 24 }} xs={{ span: 24 }} xxl={{ span: 16 }}>
+        <Col
+          className="description"
+          xl={{ span: 16 }}
+          md={{ span: 24 }}
+          xs={{ span: 24 }}
+          xxl={{ span: 16 }}
+        >
           <div className="header-card" style={{ marginTop: 10 }}>
-            <div className="status">
-              {data?.NFTType}
-            </div>
-            <div className="cancel">
-              Cancel
-            </div>
+            <div className="status">{data?.NFTType}</div>
+            <div className="cancel">Cancel</div>
           </div>
 
-          <div className="name">
-            {data?.title}
-          </div>
+          <div className="name">{data?.title}</div>
           <div className="number">
-            {price} LUCKY {' '}
-            <img src={Token} alt="" />
+            {price} LUCKY <img src={Token} alt="" />
           </div>
-          <div style={{ display: "flex" }}>
+          <div style={{ display: 'flex' }}>
             <div style={{ color: '#AFBAC5', fontWeight: 600 }}>ID:</div>
             <div className="number">{getCompactString(data?.TXHash, 6)}</div>
           </div>
 
-          {data?.description && <div className="content">
-            {data?.description}
-          </div>}
+          {data?.description && (
+            <div className="content">{data?.description}</div>
+          )}
 
           <div className="organize">
             <img src={Luckyswap} />
@@ -92,4 +112,3 @@ export default function OnSaleCard({ data, }: any) {
     </CartStyled>
   )
 }
-
