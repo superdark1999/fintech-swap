ed4a7fcd5e6e6d3c8f8606121691f768c9e46739
dinh.nguyen <dinh.nguyen@vexere.com>
2021-07-08 15:24:04 +0700

    update info

--- src/views/Nft/components/DetailArtWork/BiddingTable.tsx
+++ src/views/Nft/components/DetailArtWork/BiddingTable.tsx
@@ -21,74 +21,80 @@ import { useHistory } from 'react-router-dom'
 import ButtonProccesing from 'components-v2/Button/btnProcessing'
 
 const BiddingTable = ({
-    NFTInfo,
-    bids,
-    refreshingAfterCancelBid,
-    onSetProccessing,
-  }: any) => {
-    const { account } = useActiveWeb3React()
-    const [isProcessing, setIsProccessing] = useState(false)
-    const { buyItem } = useArtworkServices()
-    const marketServicesMethod = useMarketServices()
-    const history = useHistory()
-  
-    const onCancelBidToken = (record: any) => () => {
-      setIsProccessing(true)
-      if (marketServicesMethod) {
-        marketServicesMethod
-          ?.cancelBidToken(NFTInfo?.tokenId)
-          .then(() => {
-            onSetProccessing(true)
-            _.debounce(() => {
-              refreshingAfterCancelBid && refreshingAfterCancelBid()
-            }, 30000)()
-          })
-          .catch(() => {
-            setIsProccessing(false)
-          })
-      }
+  NFTInfo,
+  bids,
+  refreshingAfterCancelBid,
+  onSetProccessing,
+}: any) => {
+  const { account } = useActiveWeb3React()
+  const [isProcessing, setIsProccessing] = useState(false)
+  const { buyItem } = useArtworkServices()
+  const marketServicesMethod = useMarketServices()
+  const history = useHistory()
+
+  const onCancelBidToken = (record: any) => () => {
+    setIsProccessing(true)
+    if (marketServicesMethod) {
+      marketServicesMethod
+        ?.cancelBidToken(NFTInfo?.tokenId)
+        .then(() => {
+          onSetProccessing(true)
+          _.debounce(() => {
+            refreshingAfterCancelBid && refreshingAfterCancelBid()
+          }, 30000)()
+        })
+        .catch(() => {
+          setIsProccessing(false)
+        })
     }
-    const confirmSellToken = (record: any) => () => {
-      if (!account) {
-        return alert('Unblock your wallet to confirm this item')
-      }
-      if (isProcessing) return
-      setIsProccessing(true)
-      const tokenId = NFTInfo?.tokenId
-      if (tokenId && record?.address) {
-        marketServicesMethod
-          ?.sellTokenToBidUser(tokenId, record?.address)
-          .then((dt) => {
-            if (dt?.hash) {
-              buyItem({
-                id: NFTInfo?._id,
-                walletAddress: record?.address,
+  }
+  const confirmSellToken = (record: any) => () => {
+    if (!account) {
+      return alert('Unblock your wallet to confirm this item')
+    }
+    if (isProcessing) return
+    setIsProccessing(true)
+    const tokenId = NFTInfo?.tokenId
+    if (tokenId && record?.address) {
+      marketServicesMethod
+        ?.sellTokenToBidUser(tokenId, record?.address)
+        .then((dt) => {
+          if (dt?.hash) {
+            buyItem({
+              id: NFTInfo?._id,
+              walletAddress: record?.address,
+            })
+              .then(({ status }) => {
+                if (status == 200) {
+                  history.push('/my-profile/mycollection/checkingReadyToBuy')
+                }
               })
-                .then(({ status }) => {
-                  if (status == 200) {
-                    history.push('/my-profile/mycollection/checkingReadyToBuy')
-                  }
-                })
-                .catch(() => {
-                  setIsProccessing(false)
-                })
-            }
-          })
-          .catch(() => {
-            setIsProccessing(false)
-          })
-      }
+              .catch(() => {
+                setIsProccessing(false)
+              })
+          }
+        })
+        .catch(() => {
+          setIsProccessing(false)
+        })
     }
-    const columnBidding =
-  
-      NFTInfo?.ownerWalletAddress === account
-        ? [
+  }
+  const columnBidding =
+    NFTInfo?.ownerWalletAddress === account
+      ? [
           {
             title: 'Address',
             dataIndex: 'address',
             width: 100,
             render: (address: string) => (
-              <a className="value" href={window.location.origin+`/user-profile/${address}/onstore/readyToSell`} target="_blank">
+              <a
+                className="value"
+                href={
+                  window.location.origin +
+                  `/user-profile/${address}/onstore/readyToSell`
+                }
+                target="_blank"
+              >
                 {getCompactString(address, 6)}
               </a>
             ),
@@ -120,13 +126,20 @@ const BiddingTable = ({
             width: 100,
           },
         ]
-        : [
+      : [
           {
             title: 'Address',
             dataIndex: 'address',
             width: 100,
             render: (address: string) => (
-              <a className="value" href={window.location.origin+`/user-profile/${address}/onstore/readyToSell`} target="_blank">
+              <a
+                className="value"
+                href={
+                  window.location.origin +
+                  `/user-profile/${address}/onstore/readyToSell`
+                }
+                target="_blank"
+              >
                 {getCompactString(address, 6)}
               </a>
             ),
@@ -167,14 +180,14 @@ const BiddingTable = ({
             width: 100,
           },
         ]
-    return (
-      <TableStyled
-        columns={columnBidding}
-        dataSource={bids}
-        size="middle"
-        scroll={{ x: 300, y: 300 }}
-      />
-    )
-  }
+  return (
+    <TableStyled
+      columns={columnBidding}
+      dataSource={bids}
+      size="middle"
+      scroll={{ x: 300, y: 300 }}
+    />
+  )
+}
 
 export default BiddingTable
--- src/views/Nft/components/DetailArtWork/NFTInformation.tsx
+++ src/views/Nft/components/DetailArtWork/NFTInformation.tsx
@@ -10,69 +10,71 @@ import _ from 'lodash'
 const NFTInformation = ({NFTDetail}:any)=>{
     const { account, chainId } = useActiveWeb3React()
     return(
-        <DetailTabpane>
-                <div className="group-info">
-                  <div className="info">
-                    <div className="title">NFT Contract ID:</div>
-                    <a
-                      className="value"
-                      href={embedTokenIdLinkBSCScan(
-                        NFTDetail.tokenId,
-                        NFTDetail?.contractAddress,
-                        chainId,
-                      )}
-                      target="_blank"
-                    >
-                      {getCompactString(NFTDetail?.contractAddress, 6)}
-                    </a>
-                  </div>
-                  <div className="info">
-                    <div className="title">Token ID:</div>
-                    <a
-                      className="value"
-                      href={embedTokenIdLinkBSCScan(
-                        NFTDetail.tokenId,
-                        NFTDetail?.contractAddress,
-                        chainId,
-                      )}
-                      target="_blank"
-                    >
-                      {NFTDetail && NFTDetail.tokenId}
-                    </a>
-                  </div>
-                </div>
-                <div className="group-info">
-                  <div className="info">
-                    <div className="title">Creator's Adress:</div>
-                    <a
-                      className="value"
-                      href={`/user-profile/${NFTDetail?.createdBy?.walletAddress}/onstore/readyToSell`}
-                      target="_blank"
-                    >
-                      {getCompactString(NFTDetail?.createdBy?.walletAddress, 6)}
-                    </a>
-                  </div>
-                  <div className="info">
-                    <div className="title">Owner Adress:</div>
-                    <a
-                      className="value"
-                      href={`/user-profile/${NFTDetail?.ownerWalletAddress}/onstore/readyToSell`}
-                      target="_blank"
-                    >
-                      {getCompactString(NFTDetail?.ownerWalletAddress, 6)}
-                    </a>
-                  </div>
-                  {NFTDetail.contentInfo && <div className="info">
-                    <div className="title">Dimensions:</div>
-                    <a
-                      className="value"
-                      href='#'
-                    >
-                      <span>{NFTDetail?.contentInfo?.width}x{NFTDetail?.contentInfo?.height}</span>
-                    </a>
-                  </div>}
-                </div>
-              </DetailTabpane>
+      <DetailTabpane>
+      <div className="group-info">
+        <div className="info">
+          <div className="title">NFT Contract ID:</div>
+          <a
+            className="value"
+            href={embedTokenIdLinkBSCScan(
+              NFTDetail.tokenId,
+              NFTDetail?.contractAddress,
+              chainId,
+            )}
+            target="_blank"
+          >
+            {getCompactString(NFTDetail?.contractAddress, 6)}
+          </a>
+        </div>
+        <div className="info">
+          <div className="title">Token ID:</div>
+          <a
+            className="value"
+            href={embedTokenIdLinkBSCScan(
+              NFTDetail.tokenId,
+              NFTDetail?.contractAddress,
+              chainId,
+            )}
+            target="_blank"
+          >
+            {NFTDetail && NFTDetail.tokenId}
+          </a>
+        </div>
+      </div>
+      <div className="group-info">
+        <div className="info">
+          <div className="title">Creator's Adress:</div>
+          <a
+            className="value"
+            href={`/user-profile/${NFTDetail?.createdBy?.walletAddress}/onstore/readyToSell`}
+            target="_blank"
+          >
+            {getCompactString(NFTDetail?.createdBy?.walletAddress, 6)}
+          </a>
+        </div>
+        <div className="info">
+          <div className="title">Owner Adress:</div>
+          <a
+            className="value"
+            href={`/user-profile/${NFTDetail?.ownerWalletAddress}/onstore/readyToSell`}
+            target="_blank"
+          >
+            {getCompactString(NFTDetail?.ownerWalletAddress, 6)}
+          </a>
+        </div>
+        {NFTDetail.contentInfo && (
+          <div className="info">
+            <div className="title">Dimensions:</div>
+            <a className="value" href="#">
+              <span>
+                {NFTDetail?.contentInfo?.width}x
+                {NFTDetail?.contentInfo?.height}
+              </span>
+            </a>
+          </div>
+        )}
+      </div>
+    </DetailTabpane>
     )
 }
 
