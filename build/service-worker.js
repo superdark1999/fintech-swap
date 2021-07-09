c04014798bf2e440be3be757c9fe16fa6496caba
luckyswap.finance@gmail.com <luckyswap.finance@gmail.com>
2021-07-09 09:33:33 +0700

    deploy

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.62e977ac7416470339b73d19cd19abdb.js"
+  "/precache-manifest.5d5af9487ddbb14f8f8a4e61eca4a2e4.js"
 );
 
 self.addEventListener('message', (event) => {
