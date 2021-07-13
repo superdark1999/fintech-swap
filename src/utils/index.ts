0a9bfa1594d4c482dfc21823cd3edba41d52369d
dinh.nguyen <dinh.nguyen@vexere.com>
2021-07-13 21:51:14 +0700

    submit code

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -65,23 +65,23 @@ export const embedTokenIdLinkBSCScan = (tokenId:number,contractAddress:string,ch
   return `${url + contractAddress +'?a='+tokenId}`
 }
 
-export const SUPPORT_CHAIN_IDS:Array<ChainId> = [97]
+export const SUPPORT_CHAIN_IDS:Array<ChainId> = [56]
 
 export const binanceConfig = {
-  chainId: '0x61',
+  chainId: '0x38',
   rpcUrls: [
-    'https://data-seed-prebsc-1-s1.binance.org:8545/',
+    'https://bsc-dataseed.binance.org/',
   ],
-  chainName: 'Binance SmartChain Testnet',
+  chainName: 'Binance SmartChain Mainnet',
   nativeCurrency: {
-    name: 'TestnetBNB',
+    name: 'MainnetBNB',
     symbol: 'BNB', // 2-6 characters long
     decimals: 18,
   },
-  blockExplorerUrls: ['https://testnet.bscscan.com'],
+  blockExplorerUrls: ['https://bscscan.com'],
 }
 
-export const binaceText = 'bsc-testnet'
+export const binaceText = 'bsc-mainnet'
 
 export const handleAlertMessage = (message:string)=>{
   if(message==`Error: [ethjs-query] while formatting outputs from RPC '{"value":{"code":-32603,"data":{"code":-32000,"message":"transaction underpriced"}}}'`){
