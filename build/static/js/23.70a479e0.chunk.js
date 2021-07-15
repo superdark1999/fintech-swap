3c1d677dbac312a9ded85cbff10ed0897bd93360
Mac <mac@Macs-MacBook-Pro.local>
2021-07-15 11:24:18 +0700

    new build

--- build/static/js/23.70a479e0.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpluckyswap-exchange-v2"]=this["webpackJsonpluckyswap-exchange-v2"]||[]).push([[23],{1514:function(c,p){}}]);
-//# sourceMappingURL=23.70a479e0.chunk.js.map
---
+++ build/static/js/24.80561bc3.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpluckyswap-exchange-v2"]=this["webpackJsonpluckyswap-exchange-v2"]||[]).push([[24],{1684:function(c,p){}}]);
+//# sourceMappingURL=24.80561bc3.chunk.js.map
