3ab13d1b196a793c45ee1217f5ae2269c1ea6581
Khiem Nguyen <kh1em9800@gmail.com>
2021-07-18 00:34:57 +0700

    styled menu

--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -2,7 +2,6 @@ import { Web3Provider } from '@ethersproject/providers'
 import { ChainId } from '@luckyswap/v2-sdk'
 import { InjectedConnector } from '@web3-react/injected-connector'
 import { WalletConnectConnector } from '@web3-react/walletconnect-connector'
-import { WalletLinkConnector } from '@web3-react/walletlink-connector'
 import getLibrary from 'utils/getLibrary'
 import { BscConnector } from './bsc/bscConnector'
 import { NetworkConnector } from './NetworkConnector'
@@ -10,15 +9,12 @@ import { ALL_SUPPORTED_CHAIN_IDS } from '../constants/index'
 
 const RPC = {
   [ChainId.MATIC]: 'https://rpc-mainnet.maticvigil.com',
-  [ChainId.MATIC]:
-    'https://apis.ankr.com/e22bfa5f5a124b9aa1f911b742f6adfe/c06bb163c3c2a10a4028959f4d82836d/polygon/full/main',
   [ChainId.MATIC_TESTNET]: 'https://rpc-mumbai.matic.today',
   [ChainId.MAINNET]: 'https://bsc-dataseed.binance.org/',
   [ChainId.BSCTESTNET]: 'https://data-seed-prebsc-2-s3.binance.org:8545',
 }
 
 export const NETWORK_CHAIN_ID: number = parseInt('56' ?? '56')
-
 
 export const network = new NetworkConnector({
   urls: RPC,
