182985a52609c23f470463f4eb5ba722953c7143
Mac <mac@Macs-MacBook-Pro.local>
2021-07-24 18:05:30 +0700

    new block

--- src/views/Lottery/components/HowItWorks.tsx
+++ src/views/Lottery/components/HowItWorks.tsx
@@ -17,41 +17,197 @@ const LayoutWrapper = styled.div`
   text-align: center;
 `
 
-const StyledHeading = styled(Heading)`
-  margin: 16px 0;
+const SectionTitle = styled.div`
+
 `
 
-const StyledImage = styled(Image)`
-  align-self: center;
+const TitleMain = styled.h2`
+  font-size: 40px;
+  line-height: 50px;
+  margin-bottom: 13px;
+  color: #fff;
+  font-weight: 700;
+  text-transform: capitalize;
 `
 
-const StyledLink = styled(Link)`
-  align-self: center;
-  margin-top: 16px;
-  border: 1px solid #f3c111;
-  background-color: transparent;
-  color: #f3c111;
-  padding: 10px 20px;
-  border-radius: 14px;
+const SubTitle = styled.p`
+  max-width: 650px;
+  font-size: 16px;
+  line-height: 26px;
+  color: #fff;
+  margin: 0 auto;
+`
 
-  &:hover {
-    color: #ecf023;
+const BoxItem = styled.h2`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-gap: 20px;
+  margin-top: 30px;
+
+  @media (min-width: 768px) {
+    grid-template-columns: repeat(3, 1fr);
   }
 `
 
+const Item = styled.div`
+  text-align: left;
+  overflow: hidden;
+  position: relative;
+  margin-bottom: 30px;
+  background: #f4c706;
+  box-shadow: 0px 30px 20px 0px rgb(0 37 93 / 15%);
+  border-radius: 15px;
+  transition: all 0.3s ease-in;
+  cursor: pointer;
+
+  &:after {
+    position: absolute;
+    content: "";
+    background: linear-gradient(to right,#ffd009,#f97503);
+    width: 0%;
+    height: 0%;
+    top: 50%;
+    left: 50%;
+    bottom: 0px;
+    opacity: 0;
+    transform: translate(-50%, -50%);
+    transition: all 0.3s ease-in;
+    border-radius: 50%;
+  }
+
+  &:hover {
+    transform: translateY(0px) !important;
+
+    &:after {
+      width: 100%;
+      height: 100%;
+      opacity: 1;
+      border-radius: 0%;
+    }
+
+    .btn-step {
+      color: #1a1a1a;
+
+      &:before {
+        background-color: #fce307;
+      }
+    }
+  }
+`
+
+const InerItem = styled.div`
+  width: 100%;
+  position: relative;
+  z-index: 99;
+  padding: 30px 22px;
+  transition: 0.3s ease-in;
+`
+
+const Title = styled.h3`
+  color: #1a1a1a;
+  font-size: 24px;
+  font-weight: 600;
+  margin-bottom: 15px;
+`
+
+const Desc = styled.p`
+  font-size: 16px;
+  color: #464646;
+`
+
+const Step = styled.span`
+	display: flex;
+  justify-content: center;
+  margin-left: auto;
+  width: 80px;
+	padding: 10px 0;
+	border-radius: 10rem;
+	color: #fff;
+	text-transform: uppercase;
+	font-size: 1rem;
+	transition: all .3s;
+	position: relative;
+	overflow: hidden;
+	z-index: 1;
+
+	&:after {
+		content: '';
+		position: absolute;
+		bottom: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background-color: #c5892e;
+		border-radius: 10rem;
+		z-index: -2;
+	}
+
+	&:before {
+		content: '';
+		position: absolute;
+		bottom: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		transition: all .3s;
+		border-radius: 10rem;
+		z-index: -1;
+	}
+`
+
 const HowItWorks = () => {
-  const TranslateString = useI18n()
+  // const TranslateString = useI18n()
 
   return (
     <LayoutWrapper>
-      <StyledHeading size="lg" as="h3" color="rgb(243, 193, 17)">
-        {TranslateString(632, 'How it works')}
-      </StyledHeading>
-      <Text fontSize="16px">
-        Spend LUCKY to buy tickets, contributing to the lottery pot.
-        <br /> Win prizes if 2, 3, or 4 of your ticket numbers match the winning numbers and their exact order!
-      </Text>
-      <StyledLink href="https://docs.luckyswap.io">{TranslateString(610, 'Read more')}</StyledLink>
+      <SectionTitle>
+        <TitleMain>How to Play</TitleMain>
+        <SubTitle>If the digits on your tickets match the winning numbers in the correct order, you win a portion of the prize pool Simple!</SubTitle>
+      </SectionTitle>
+
+      <BoxItem>
+        <Item>
+          <InerItem>
+            <Step className="btn-step">  
+              step 1
+            </Step>
+
+            <Title>LUCKY per ticket</Title>
+
+            <Desc>
+              Prices are set when the round starts, equat to 5 USD in LUCKY per ticket.
+            </Desc>
+          </InerItem>
+        </Item>
+
+        <Item>
+          <InerItem>
+            <Step className="btn-step">  
+              step 2
+            </Step>
+
+            <Title>Wait for the Draw</Title>
+
+            <Desc>
+              There are two draws every day: once every 24h.
+            </Desc>
+          </InerItem>
+        </Item>
+
+        <Item>
+          <InerItem>
+            <Step className="btn-step">  
+              step 3
+            </Step>
+
+            <Title>Check for Prizes</Title>
+
+            <Desc>
+              Once the round&apos;s over, come back to the page and check to see if you&apos;ve won!
+            </Desc>
+          </InerItem>
+        </Item>
+      </BoxItem>
     </LayoutWrapper>
   )
 }
